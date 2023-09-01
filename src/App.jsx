23a0acc2f97f3f96fd0b8516a5f438ec74161669
Youssef303M <youssef303m@gmail.com>
2023-09-01 13:56:16 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import { About, Contact, Home, Portfolio, Testimonials } from "./pages";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Portfolio } from "./Portfolio";
+export { default as Testimonials } from "./Testimonials";
+export { default as Contact } from "./Contact";
