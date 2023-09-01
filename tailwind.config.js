f467fc34a564cf7b08a5819fd5bdf28ba646fffe
Youssef303M <youssef303m@gmail.com>
2023-09-01 17:06:38 +0300

    Boilerplate CSS and add ubuntu font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: ["ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
