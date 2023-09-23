fa4ebb50c63515777b74d93a83e6b76fd7aaf1d0
Youssef303M <youssef303m@gmail.com>
2023-09-23 08:43:34 +0300

    Finished portfolio skeleton

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,36 @@
-import React from "react";
-
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section className="min-h-screen flex items-center justify-center py-[140px]">
+      <div className="container flex flex-col items-center">
+        <h2 className="text-[48px] sm:text-[65px] font-[600]">
+          My <span className="text-primary">Works</span>
+        </h2>
+        <p className="mt-2 text-[18px] sm:text-[24px] text-paragraph">
+          Take a look at some of my projects
+        </p>
+        <div className="mt-16 grid xl:grid-cols-2 2xl:grid-cols-3 gap-8">
+          <a href="#">
+            <div className="bg-gray-300 w-[340px] sm:w-[400px] aspect-[16/9] rounded-[10px]"></div>
+          </a>
+          <a href="#">
+            <div className="bg-gray-300 w-[340px] sm:w-[400px] aspect-[16/9] rounded-[10px]"></div>
+          </a>
+          <a href="#">
+            <div className="bg-gray-300 w-[340px] sm:w-[400px] aspect-[16/9] rounded-[10px]"></div>
+          </a>
+          <a href="#">
+            <div className="bg-gray-300 w-[340px] sm:w-[400px] aspect-[16/9] rounded-[10px]"></div>
+          </a>
+          <a href="#">
+            <div className="bg-gray-300 w-[340px] sm:w-[400px] aspect-[16/9] rounded-[10px]"></div>
+          </a>
+          <a href="#">
+            <div className="bg-gray-300 w-[340px] sm:w-[400px] aspect-[16/9] rounded-[10px]"></div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
