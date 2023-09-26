0fc0bb98f71298c8aff046cb00d0410f00e6da39
Youssef303M <youssef303m@gmail.com>
2023-09-26 13:25:40 +0300

    Finished Contact me Page, Not functioning yet

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,12 @@
+// Import icons
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
+import { HiOutlineExternalLink } from "react-icons/hi";
+
 const contact = () => {
   return (
     <section className="min-h-screen w-screen flex items-center justify-center">
-      <div className="container px-[10px] flex flex-col items-center justify-center mt-[100px]">
+      <div className="container px-[10px] flex flex-col items-center justify-center my-[100px]">
         <h2 className="text-[42px] sm:text-[48px] lg:text-[52px] 2xl:text-[65px] font-[600]">
           <span className="text-primary">Contact</span> Me
         </h2>
@@ -61,9 +66,45 @@ const contact = () => {
             <h3 className="text-primary text-[36px] font-[500]">
               Social Media
             </h3>
-            <p className="text-paragraph text-[18px] mt-1">
+            <p className="text-paragraph text-[18px] mt-1 mb-8">
               Find me on Social Media!
             </p>
+            {/* Socials */}
+            <div className="flex flex-col gap-8 lg:gap-4">
+              {/* LinkedIN */}
+              <a
+                href="ttps://www.linkedin.com/in/youssef303m/"
+                className="text-[18px] flex gap-4 items-center hover:text-primary duration-main"
+              >
+                <BsLinkedin size={25} />{" "}
+                <span className="flex items-center mt-[-4px]">
+                  youssef303m{" "}
+                  <HiOutlineExternalLink className="ml-1 mb-[-4px]" />
+                </span>
+              </a>
+              {/* Github */}
+              <a
+                href="https://github.com/youssef303m"
+                className="text-[18px] flex gap-4 items-center hover:text-primary duration-main"
+              >
+                <BsGithub size={25} />{" "}
+                <span className="flex items-center mt-[-4px]">
+                  youssef303m{" "}
+                  <HiOutlineExternalLink className="ml-1 mb-[-4px]" />
+                </span>
+              </a>
+              {/* Twitter */}
+              <a
+                href="https://twitter.com/youssef303m"
+                className="text-[18px] flex gap-4 items-center hover:text-primary duration-main"
+              >
+                <FaXTwitter size={25} />{" "}
+                <span className="flex items-center mt-[-4px]">
+                  youssef303m{" "}
+                  <HiOutlineExternalLink className="ml-1 mb-[-4px]" />
+                </span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
